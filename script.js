3d74cae3b57a960ef2c2a2185f1513cc9d45a72a
ab1nash10 <ab1nashkumar9815@gmail.com>
2024-01-11 12:44:08 +0530

    done with the analog clock design

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let hrHand = document.querySelector(".hrHand");
+let minHand = document.querySelector(".minHand");
+let secHand = document.querySelector(".secHand");
 
+setInterval(() => {
+  let date = new Date();
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+
+  let hrRotate = (hr / 12) * 360 + (min / 60) * 30;
+  hrRotate = Math.round(hrRotate * 10) / 10;
+  let minRotate = (min / 60) * 360;
+  let secRotate = (sec / 60) * 360;
+
+  hrHand.style.transform = `rotate(${hrRotate}deg)`;
+  minHand.style.transform = `rotate(${minRotate}deg)`;
+  secHand.style.transform = `rotate(${secRotate}deg)`;
+}, 1000);
