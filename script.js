5772ba0bb71780d2955daf804380f94aefc52a65
ab1nash10 <ab1nashkumar9815@gmail.com>
2024-01-12 23:08:47 +0530

    done with the ui part

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let hrHand = document.querySelector(".hrHand");
 let minHand = document.querySelector(".minHand");
 let secHand = document.querySelector(".secHand");
+let digitalClock = document.querySelector(".digitalClock");
 
 setInterval(() => {
   let date = new Date();
+
   let hr = date.getHours();
   let min = date.getMinutes();
   let sec = date.getSeconds();
@@ -16,4 +18,6 @@ setInterval(() => {
   hrHand.style.transform = `rotate(${hrRotate}deg)`;
   minHand.style.transform = `rotate(${minRotate}deg)`;
   secHand.style.transform = `rotate(${secRotate}deg)`;
+
+  digitalClock.innerText = date.toLocaleTimeString();
 }, 1000);
